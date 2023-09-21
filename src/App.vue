fc12bc4559e2591167abec9764c96fefc20901f3
tarbz2 <12904993+tarbz2@users.noreply.github.com>
2023-09-21 09:59:33 -0300

    Se corrige README y se agrega configuración de entornos dev y producción

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <script setup>
+import { onMounted } from 'vue';
 import NavBar from './components/NavBar.vue';
 import TheFooter from './components/TheFooter.vue';
+
+onMounted(() => {
+  console.log(import.meta.env.VITE_ROOT_API)
+  console.log(import.meta.env.VITE_TITLE)
+})
+
 
 </script>
 
 <template>
   <header>
     <NavBar />
+    
   </header>
 
   <RouterView />
+
+  <footer>
     <TheFooter />
+  </footer>
+
 </template>
 
 <style>
