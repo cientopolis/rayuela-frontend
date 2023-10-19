cf2050c01dc7ef5cf1b05b8ef21cdd12cbc4a46d
tarbz2 <12904993+tarbz2@users.noreply.github.com>
2023-10-19 01:32:04 -0300

    Se agrega dashboard base

--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('../views/DashboardView.vue')
     }
   ]
 })
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Dashboard from '../components/TheDashboard.vue'
+</script>
+
+<template>
+  <main>
+    <Dashboard />
+  </main>
+</template>
